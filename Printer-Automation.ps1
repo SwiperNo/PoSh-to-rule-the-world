b2051fd9fa844cfce762e664bbe2c95c8e3bed88
SwiperNo <39900853+SwiperNo@users.noreply.github.com>
2019-04-05 12:48:45 -0500

    Add files via upload

---
+++ Printer-Automation.ps1
@@ -0,0 +1,17 @@
+﻿$portName = "TCPPort10.157.128.161"
+$printDriverName = "Edinburg HP"
+ 
+ 
+$portExists = Get-Printerport -Name $portname -ErrorAction SilentlyContinue
+ 
+if (-not $portExists) {
+  Add-PrinterPort -name $portName -PrinterHostAddress "10.157.128.161"
+}
+ 
+$printDriverExists = Get-PrinterDriver -name $printDriverName -ErrorAction SilentlyContinue
+ 
+if ($printDriverExists) {
+    Add-Printer -Name "Edinburg HP" -PortName $portName -DriverName $printDriverName
+}else{
+    Write-Warning "Printer Driver not installed"
+}
